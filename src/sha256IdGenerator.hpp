ef4af47f6b0ba7486ff053825469ed308e433485
Szymon Michniak <s.michniak@icloud.com>
2021-02-16 23:56:44 +0100

    SHA256 generator

--- src/sha256IdGenerator.hpp
+++ src/sha256IdGenerator.hpp
@@ -6,9 +6,25 @@
 
 class Sha256IdGenerator : public IdGenerator {
 public:
-    virtual PageId generateId(std::string const& /*content*/) const
-    {
-        return PageId("");
+    virtual PageId generateId(std::string const& content) const {
+        int static const HASH_LENGTH = 65;
+        char const READ_MODE[2] = "r";
+        char hash_buffer[HASH_LENGTH];
+
+        std::string hash_command = "echo -n \"" + content + "\" | sha256sum";
+        FILE* stream = popen(hash_command.c_str(), READ_MODE);
+        if (!stream) {
+            exit(1); // Failed to open pipe to to the other process
+        }
+
+        if (!fgets(hash_buffer, HASH_LENGTH, stream)) {
+            pclose(stream);
+            exit(1); // Failed to read the hash from the stream
+        }
+
+        pclose(stream);
+
+        return PageId(hash_buffer);
     }
 };
 
--- tests/sha256Test.cpp
+++ tests/sha256Test.cpp
@@ -17,6 +17,8 @@ int main()
 {
     testSha256("Ala ma kota\n", "c51bc001db0206126e1681ba88497ce583f077a92e427e4f62da96b691d28813");
     testSha256("Zawartość naprawdę naprawdę naprawdę wielkiego pliku 1234567890\n", "69bddbdc52992ae9952d3368d48bfe0517ce346d6040e495de3542926294498b");
+    testSha256("Ala ma kota", "124bfb6284d82f3b1105f88e3e7a0ee02d0e525193413c05b75041917022cd6e");
+    testSha256("Ala\nma\nkota", "c824ad9ef06c38fcf6ed303013bb8d105407aa66b1d4848d811637027d6cd001");
 
     return 0;
 }
